757db855fe581c6ff604fc9e23f4dc8827e047c6
Sofia <speerbuccus@gmail.com>
2020-12-15 15:06:11 +0100

    2nd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarsComponent } from './mars/mars.component';
+import { ListImagesComponent } from './list-images/list-images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarsComponent,
+    ListImagesComponent
   ],
   imports: [
     BrowserModule,
