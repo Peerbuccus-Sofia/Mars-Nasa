2318ce42f5ffbfc5c9f46f2f9631b498437461d2
BalthazarGrimaldi <balthazar.grimaldi@gmail.com>
2020-12-15 17:17:53 +0100

    creation de list-images-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarsComponent } from './mars/mars.component';
 import { ListImagesComponent } from './list-images/list-images.component';
+import { ListImagesItemComponent } from './list-images-item/list-images-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MarsComponent,
-    ListImagesComponent
+    ListImagesComponent,
+    ListImagesItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-images-item/list-images-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-list-images-item',
+  templateUrl: './list-images-item.component.html',
+  styleUrls: ['./list-images-item.component.css']
+})
+export class ListImagesItemComponent implements OnInit {
+  @Input() marsImgSrc: String;
+  constructor() { 
+    this.marsImgSrc = "";
+  }
+
+  ngOnInit(): void {
+  }
+
+}
